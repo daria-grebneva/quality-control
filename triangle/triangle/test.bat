d34a766193dd5bb29cb96173dbf2037430fbd925
daria-grebneva <greda2598@gmail.com>
2018-09-17 00:21:11 +0300

    Изменила файл с тестами

--- triangle/triangle/test.bat
+++ triangle/triangle/test.bat
@@ -18,6 +18,24 @@ IF ERRORLEVEL 1 GOTO err
 FC output.txt test\out3.txt 
 IF ERRORLEVEL 1 GOTO err  
         
+REM равнобедренный
+%PROGRAM% test\in4.txt output.txt                                 
+IF ERRORLEVEL 1 GOTO err 
+FC output.txt test\out4.txt 
+IF ERRORLEVEL 1 GOTO err  
+
+REM обычный
+%PROGRAM% test\in5.txt output.txt                                 
+IF ERRORLEVEL 1 GOTO err 
+FC output.txt test\out5.txt 
+IF ERRORLEVEL 1 GOTO err                               
+                            
+REM не треугольник
+%PROGRAM% test\in6.txt output.txt                                 
+IF ERRORLEVEL 1 GOTO err 
+FC output.txt test\out6.txt 
+IF ERRORLEVEL 1 GOTO err  
+                     
 ECHO Program testing succeeded :-)                  
 EXIT                                                
 :err                                                
