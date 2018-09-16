332a20e5958a1284562558d5a997703109e23724
daria-grebneva <greda2598@gmail.com>
2018-09-17 00:20:00 +0300

    Первая лабораторная

---
+++ triangle/triangle/test.bat
@@ -0,0 +1,25 @@
+set PROGRAM="%~1"
+                                                                
+REM неверное количество параметров
+%PROGRAM% test\in1.txt output.txt                                 
+IF NOT ERRORLEVEL 1 GOTO err 
+FC output.txt test\out1.txt 
+IF NOT ERRORLEVEL 1 GOTO err  
+
+REM неверный пользовательский ввод  
+%PROGRAM% test\in2.txt output.txt                                 
+IF NOT ERRORLEVEL 1 GOTO err 
+FC output.txt test\out2.txt 
+IF NOT ERRORLEVEL 1 GOTO err  
+           
+REM равносторонний
+triangle.exe test\in3.txt output.txt                                 
+IF ERRORLEVEL 1 GOTO err 
+FC output.txt test\out3.txt 
+IF ERRORLEVEL 1 GOTO err  
+        
+ECHO Program testing succeeded :-)                  
+EXIT                                                
+:err                                                
+ECHO Program testing failed :-(                     
+EXIT
